e141c934114d50acd6438e0025e317ae1b124f56
saraaruiz <saruizdi@gmail.com>
2023-09-24 21:09:23 -0500

    [create] pag nosotros Selecto

---
+++ sitioSelecto/2_pagNosotros/javascript/nosotros.js
@@ -0,0 +1,11 @@
+//Efecto Navbar responsive//
+
+function openNav() {
+    document.getElementById("responsive-menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("responsive-menu").style.width = "0%";
+}
+
+

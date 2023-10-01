a6965f2774f1fe18c1295c9c4fdb9baf99d2409f
Luisa Fernanda Castaño Vanegas <luisa.castano40@eia.edu.co>
2023-10-01 12:36:35 -0500

    [UPDATE] responsive y js pag contáctenos

--- sitioSelecto/3_pagContactanos/javascript/contactanos.js
+++ sitioSelecto/3_pagContactanos/javascript/contactanos.js
@@ -1,13 +1,47 @@
 //Efecto Navbar responsive//
-function openNav() {
-    document.getElementById("responsive-menu").style.width = "100%";
-    document.querySelector('.d-none').style.display = 'block';
-    document.getElementById('menuResponsive-content').style.visibility = "visible";
-    document.querySelector('.close').style.visibility = "visible";
-}
+// function openNav() {
+//     document.getElementById("responsive-menu").style.width = "100%";
+//     document.querySelector('.d-none').style.display = 'block';
+//     document.getElementById('menuResponsive-content').style.visibility = "visible";
+//     document.querySelector('.close').style.visibility = "visible";
+// }
 
-function closeNav() {
-    document.getElementById("responsive-menu").style.width = "0%";
-    document.getElementById('menuResponsive-content').style.visibility = "hidden";
-    document.querySelector('.close').style.visibility = "hidden";
+// function closeNav() {
+//     document.getElementById("responsive-menu").style.width = "0%";
+//     document.getElementById('menuResponsive-content').style.visibility = "hidden";
+//     document.querySelector('.close').style.visibility = "hidden";
+// }
+
+//Traemos los elementos del HTML//
+const selectRol = document.getElementById("rol");
+isCliente = false;
+
+//Eventos
+selectRol.addEventListener("change", mostrarInput);
+
+//Funciones
+function mostrarInput(){
+
+    const seleccion = selectRol.value;
+    const divCurriculum = document.querySelector(".curriculum");
+    const inputEmail = document.querySelector('.datos-email');
+
+    if (seleccion === "cliente") {
+        divCurriculum.style.display = "none";
+        inputEmail.style.width = "100%";
+    } else if (seleccion === "proveedor" || seleccion === "barbero") {
+        divCurriculum.style.display = "block";
+        divCurriculum.style.width = "50%";
+        inputEmail.style.width = "50%";
+
+        divCurriculum.innerHTML = '';
+        divCurriculum.innerHTML =`
+        <div class="inputCV">
+            <label for="file">
+                <i class='bx bx-cloud-upload' ></i>
+                Hoja de vida
+            </label>
+            <input id="file" type="file">
+        </div>`;
+    }
 }

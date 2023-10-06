f124c707ac60b24fa0e1b63aaa2560372964639c
Edgardavid0519 <edgardavid0519@gmail.com>
2023-10-05 22:23:41 -0500

    MaquetacionFormularioProductosCreacionBasesDatosFk

---
+++ sitioSelecto/basesDeDatosFake/baseDeDAtosServicios.js
@@ -0,0 +1,142 @@
+const servicio = [
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000
+    }
+];
---
+++ sitioSelecto/basesDeDatosFake/baseDeDatosProductos.js
@@ -0,0 +1,142 @@
+const producto = [
+    {
+        id:1,
+        categoria:"Gel",
+        imagen: "/assets/producto1.svg",
+        nombre: "Gel Red One",
+        precio: 48000
+    },
+    {
+        id:2,
+        categoria:"Cera",
+        imagen: "/assets/producto2.svg",
+        nombre: "Cera Red One",
+        precio: 48000
+    },
+    {
+        id:3,
+        categoria:"Cera",
+        imagen: "/assets/producto3.svg",
+        nombre: "Cera Mate Red One",
+        precio: 48000
+    },
+    {
+        id:4,
+        categoria:"Cerveza",
+        imagen: "/assets/producto4.svg",
+        nombre: "Cerveza Heiniken",
+        precio: 4000
+    },
+    {
+        id:5,
+        categoria:"Afeitadora",
+        imagen: "/assets/producto5.svg",
+        nombre: "Shaver Kemel",
+        precio: 140000
+    },
+    {
+        id:1,
+        categoria:"Gel",
+        imagen: "/assets/producto1.svg",
+        nombre: "Gel Red One",
+        precio: 48000
+    },
+    {
+        id:2,
+        categoria:"Cera",
+        imagen: "/assets/producto2.svg",
+        nombre: "Cera Red One",
+        precio: 48000
+    },
+    {
+        id:3,
+        categoria:"Cera",
+        imagen: "/assets/producto3.svg",
+        nombre: "Cera Mate Red One",
+        precio: 48000
+    },
+    {
+        id:4,
+        categoria:"Cerveza",
+        imagen: "/assets/producto4.svg",
+        nombre: "Cerveza Heiniken",
+        precio: 4000
+    },
+    {
+        id:5,
+        categoria:"Afeitadora",
+        imagen: "/assets/producto5.svg",
+        nombre: "Shaver Kemel",
+        precio: 140000
+    },
+    {
+        id:1,
+        categoria:"Gel",
+        imagen: "/assets/producto1.svg",
+        nombre: "Gel Red One",
+        precio: 48000
+    },
+    {
+        id:2,
+        categoria:"Cera",
+        imagen: "/assets/producto2.svg",
+        nombre: "Cera Red One",
+        precio: 48000
+    },
+    {
+        id:3,
+        categoria:"Cera",
+        imagen: "/assets/producto3.svg",
+        nombre: "Cera Mate Red One",
+        precio: 48000
+    },
+    {
+        id:4,
+        categoria:"Cerveza",
+        imagen: "/assets/producto4.svg",
+        nombre: "Cerveza Heiniken",
+        precio: 4000
+    },
+    {
+        id:5,
+        categoria:"Afeitadora",
+        imagen: "/assets/producto5.svg",
+        nombre: "Shaver Kemel",
+        precio: 140000
+    },
+    {
+        id:1,
+        categoria:"Gel",
+        imagen: "/assets/producto1.svg",
+        nombre: "Gel Red One",
+        precio: 48000
+    },
+    {
+        id:2,
+        categoria:"Cera",
+        imagen: "/assets/producto2.svg",
+        nombre: "Cera Red One",
+        precio: 48000
+    },
+    {
+        id:3,
+        categoria:"Cera",
+        imagen: "/assets/producto3.svg",
+        nombre: "Cera Mate Red One",
+        precio: 48000
+    },
+    {
+        id:4,
+        categoria:"Cerveza",
+        imagen: "/assets/producto4.svg",
+        nombre: "Cerveza Heiniken",
+        precio: 4000
+    },
+    {
+        id:5,
+        categoria:"Afeitadora",
+        imagen: "/assets/producto5.svg",
+        nombre: "Shaver Kemel",
+        precio: 140000
+    }
+];

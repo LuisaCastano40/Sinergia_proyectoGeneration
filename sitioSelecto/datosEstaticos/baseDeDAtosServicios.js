121783cf810c29f7dd569df36117a6692f0cf346
Luisa Fernanda Castaño Vanegas <luisa.castano40@eia.edu.co>
2023-10-06 14:01:27 -0500

    pruebas

---
+++ sitioSelecto/datosEstaticos/baseDeDAtosServicios.js
@@ -0,0 +1,161 @@
+const servicio = [
+    {
+        id:0,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:1,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:2,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000
+    },
+    {
+        id:3,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:4,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:5,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:6,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:7,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:8,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:9,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:10,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:11,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:12,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:13,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:14,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:15,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:16,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:17,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:18,
+        categoria:"Corte",
+        imagen: "/assets/servicio1.svg",
+        nombre: "Corte",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    },
+    {
+        id:19,
+        categoria:"Barba",
+        imagen: "/assets/servicio2.svg",
+        nombre: "Barba",
+        precio: 20000,
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere rem aliquid hic cupiditate atque doloremque veniam at, quia."
+    }
+];
--- sitioSelecto/datosEstaticos/baseDeDatosProductos.js
+++ sitioSelecto/datosEstaticos/baseDeDatosProductos.js
@@ -35,105 +35,105 @@ const producto = [
         precio: 140000
     },
     {
-        id:1,
+        id:6,
         categoria:"Gel",
         imagen: "/assets/producto1.svg",
         nombre: "Gel Red One",
         precio: 48000
     },
     {
-        id:2,
+        id:7,
         categoria:"Cera",
         imagen: "/assets/producto2.svg",
         nombre: "Cera Red One",
         precio: 48000
     },
     {
-        id:3,
+        id:8,
         categoria:"Cera",
         imagen: "/assets/producto3.svg",
         nombre: "Cera Mate Red One",
         precio: 48000
     },
     {
-        id:4,
+        id:9,
         categoria:"Cerveza",
         imagen: "/assets/producto4.svg",
         nombre: "Cerveza Heiniken",
         precio: 4000
     },
     {
-        id:5,
+        id:10,
         categoria:"Afeitadora",
         imagen: "/assets/producto5.svg",
         nombre: "Shaver Kemel",
         precio: 140000
     },
     {
-        id:1,
+        id:11,
         categoria:"Gel",
         imagen: "/assets/producto1.svg",
         nombre: "Gel Red One",
         precio: 48000
     },
     {
-        id:2,
+        id:12,
         categoria:"Cera",
         imagen: "/assets/producto2.svg",
         nombre: "Cera Red One",
         precio: 48000
     },
     {
-        id:3,
+        id:13,
         categoria:"Cera",
         imagen: "/assets/producto3.svg",
         nombre: "Cera Mate Red One",
         precio: 48000
     },
     {
-        id:4,
+        id:14,
         categoria:"Cerveza",
         imagen: "/assets/producto4.svg",
         nombre: "Cerveza Heiniken",
         precio: 4000
     },
     {
-        id:5,
+        id:15,
         categoria:"Afeitadora",
         imagen: "/assets/producto5.svg",
         nombre: "Shaver Kemel",
         precio: 140000
     },
     {
-        id:1,
+        id:16,
         categoria:"Gel",
         imagen: "/assets/producto1.svg",
         nombre: "Gel Red One",
         precio: 48000
     },
     {
-        id:2,
+        id:17,
         categoria:"Cera",
         imagen: "/assets/producto2.svg",
         nombre: "Cera Red One",
         precio: 48000
     },
     {
-        id:3,
+        id:18,
         categoria:"Cera",
         imagen: "/assets/producto3.svg",
         nombre: "Cera Mate Red One",
         precio: 48000
     },
     {
-        id:4,
+        id:19,
         categoria:"Cerveza",
         imagen: "/assets/producto4.svg",
         nombre: "Cerveza Heiniken",
         precio: 4000
     },
     {
-        id:5,
+        id:20,
         categoria:"Afeitadora",
         imagen: "/assets/producto5.svg",
         nombre: "Shaver Kemel",

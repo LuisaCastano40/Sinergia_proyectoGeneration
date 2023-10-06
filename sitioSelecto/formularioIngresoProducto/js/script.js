28d4303e26c1b2c836f79e1105e89d738168ffe4
Edgardavid0519 <edgardavid0519@gmail.com>
2023-10-06 00:19:54 -0500

    detallesMaquetacionFormulario

--- sitioSelecto/formularioIngresoProducto/js/script.js
+++ sitioSelecto/formularioIngresoProducto/js/script.js
@@ -6,6 +6,7 @@ const img = document.getElementById('img');
 file.addEventListener('change', e =>{
     if(e.target.files[0]){
         const reader = new FileReader();
+        alert("Archivo cargado Exitosamente")
         reader.onload = (e) =>{
             img.src = e.target.result;
         }

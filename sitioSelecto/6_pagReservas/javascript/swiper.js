182f23434e62871a88c62c205c0729739476f6e9
Edgardavid0519 <edgardavid0519@gmail.com>
2023-11-08 01:53:48 -0500

    Reservas, MedioMaquetacion y Estilos

---
+++ sitioSelecto/6_pagReservas/javascript/swiper.js
@@ -0,0 +1,23 @@
+const swiper1 = new Swiper('.swiper-Servicios', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.Siguiente-Servicios',
+          prevEl: '.Anterior-Servicios',
+        },
+      });
+
+      const swiper2 = new Swiper('.swiper-Franja', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.Siguiente-Franja',
+          prevEl: '.Anterior-Franja',
+        },
+      });
